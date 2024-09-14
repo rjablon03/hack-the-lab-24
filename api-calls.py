c9b94a121d781e77aec52b949217025cdad5c426
rjablon03 <secrets_crasser.0l@icloud.com>
2024-09-14 12:55:22 -0400

    Exit function complete

--- api-calls.py
+++ api-calls.py
@@ -49,6 +49,23 @@ def moveRat(mazeId):
         response = requests.post(url, headers=headers, json=payload)
         return response.json()
 
+def exitMaze(mazeId):
+    url = f"http://api.milabs.xyz/v1/rat/exit"
+
+    headers = {
+        'X-Api-Key': "781200B275164705"
+    }
+
+    payload = {
+        "mazeId": mazeId
+    }
+
+    if ratSurroundings(mazeId)['type'] == "Exit":
+        response = requests.post(url, headers=headers,json=payload)
+        return response
+    
+    return "Error of some sort"
+
 
 
 
